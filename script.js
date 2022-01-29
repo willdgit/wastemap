cc5de9c80e9f4ad0b92982bcaf7b4a664b4b0491
William Derksen <97864183+willdgit@users.noreply.github.com>
2022-01-28 20:03:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//StAuth10244: I William Derksen 000834722, certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
-
 document.getElementById("geolocate").onclick = function () {
     navigator.geolocation.getCurrentPosition(showPositionOnMap);
 }
